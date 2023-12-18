0e81c099cedfa4be0ec6b99630fdbebd817f6f5a
babur <babur@Babus.mynetworksettings.com>
2023-12-18 18:10:09 -0500

    Git code

---
+++ GitProject/src/java/practice/ifandelse.java
@@ -0,0 +1,17 @@
+package java.practice;
+
+public class ifandelse {
+
+	public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+        
+
+        if (num1 == num2) {
+            System.out.println("The number is same.");
+        } else {
+            System.out.println("The number is different.");
+        }
+    }
+
+}
